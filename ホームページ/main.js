7ad8c2fc2f54d61873d562255dcdf6295a1f6979
i-bu-hub <114901868+i-bu-hub@users.noreply.github.com>
2024-01-31 00:03:06 +0900

    Add files via upload

---
+++ ホームページ/main.js
@@ -0,0 +1,15 @@
+document.getElementById('proposal').onclick = function() {
+  window.location.href = 'dc2023_10.pdf';
+};
+
+document.getElementById('use').onclick = function() {
+  window.location.href = 'manual.html';
+};
+
+document.getElementById('gitHub').onclick = function() {
+  window.location.href = 'https://github.com/ikedatomoki/digital-application';
+};
+
+document.getElementById('intro').onclick = function() {
+  window.location.href = 'https://youtu.be/zdMvpjwSAMQ';
+};
